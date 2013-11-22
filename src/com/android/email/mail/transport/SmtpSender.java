496a35a564ce8d83df36352243faeedf8056b238
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-11-22 11:12:17 -0500

    'Merge into android 4.4_r1.

--- src/com/android/email/mail/transport/SmtpSender.java
+++ src/com/android/email/mail/transport/SmtpSender.java
@@ -18,11 +18,9 @@ package com.android.email.mail.transport;
 
 import android.content.Context;
 import android.util.Base64;
-import android.util.Log;
 
-import com.android.email.Email;
 import com.android.email.mail.Sender;
-import com.android.email.mail.Transport;
+import com.android.email2.ui.MailActivityEmail;
 import com.android.emailcommon.Logging;
 import com.android.emailcommon.internet.Rfc822Output;
 import com.android.emailcommon.mail.Address;
@@ -32,26 +30,22 @@ import com.android.emailcommon.mail.MessagingException;
 import com.android.emailcommon.provider.Account;
 import com.android.emailcommon.provider.EmailContent.Message;
 import com.android.emailcommon.provider.HostAuth;
+import com.android.emailcommon.utility.EOLConvertingOutputStream;
+import com.android.mail.utils.LogUtils;
 
 import java.io.IOException;
 import java.net.Inet6Address;
 import java.net.InetAddress;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Formatter;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.SSLException;
 
 /**
- * This class handles all of the protocol-level aspects of sending messages via
- * SMTP. TODO Remove dependence upon URI; there's no reason why we need it here
+ * This class handles all of the protocol-level aspects of sending messages via SMTP.
  */
 public class SmtpSender extends Sender {
 
     private final Context mContext;
-    private Transport mTransport;
+    private MailTransport mTransport;
     private String mUsername;
     private String mPassword;
 
@@ -65,33 +59,10 @@ public class SmtpSender extends Sender {
     /**
      * Creates a new sender for the given account.
      */
-    private SmtpSender(Context context, Account account) throws MessagingException {
+    public SmtpSender(Context context, Account account) {
         mContext = context;
         HostAuth sendAuth = account.getOrCreateHostAuthSend(context);
-        if (sendAuth == null || !"smtp".equalsIgnoreCase(sendAuth.mProtocol)) {
-            throw new MessagingException("Unsupported protocol");
-        }
-        // defaults, which can be changed by security modifiers
-        int connectionSecurity = Transport.CONNECTION_SECURITY_NONE;
-        int defaultPort = 587;
-
-        // check for security flags and apply changes
-        if ((sendAuth.mFlags & HostAuth.FLAG_SSL) != 0) {
-            connectionSecurity = Transport.CONNECTION_SECURITY_SSL;
-            defaultPort = 465;
-        } else if ((sendAuth.mFlags & HostAuth.FLAG_TLS) != 0) {
-            connectionSecurity = Transport.CONNECTION_SECURITY_TLS;
-        }
-        boolean trustCertificates = ((sendAuth.mFlags & HostAuth.FLAG_TRUST_ALL) != 0);
-        int port = defaultPort;
-        if (sendAuth.mPort != HostAuth.PORT_UNKNOWN) {
-            port = sendAuth.mPort;
-        }
-        mTransport = new MailTransport("IMAP");
-        mTransport.setHost(sendAuth.mAddress);
-        mTransport.setPort(port);
-        mTransport.setSecurity(connectionSecurity, trustCertificates);
-
+        mTransport = new MailTransport(context, "SMTP", sendAuth);
         String[] userInfoParts = sendAuth.getLogin();
         if (userInfoParts != null) {
             mUsername = userInfoParts[0];
@@ -100,12 +71,11 @@ public class SmtpSender extends Sender {
     }
 
     /**
-     * For testing only. Injects a different transport. The transport should
-     * already be set up and ready to use. Do not use for real code.
-     * @param testTransport The Transport to inject and use for all future
-     *            communication.
+     * For testing only.  Injects a different transport.  The transport should already be set
+     * up and ready to use.  Do not use for real code.
+     * @param testTransport The Transport to inject and use for all future communication.
      */
-    /* package */void setTransport(Transport testTransport) {
+    /* package */ void setTransport(MailTransport testTransport) {
         mTransport = testTransport;
     }
 
@@ -136,22 +106,23 @@ public class SmtpSender extends Sender {
             /*
              * TODO may need to add code to fall back to HELO I switched it from
              * using HELO on non STARTTLS connections because of AOL's mail
-             * server. It won't let you use AUTH without EHLO. We should really
-             * be paying more attention to the capabilities and only attempting
-             * auth if it's available, and warning the user if not.
+             * server. It won't let you use AUTH without EHLO.
+             * We should really be paying more attention to the capabilities
+             * and only attempting auth if it's available, and warning the user
+             * if not.
              */
             if (mTransport.canTryTlsSecurity()) {
                 if (result.contains("STARTTLS")) {
                     executeSimpleCommand("STARTTLS");
                     mTransport.reopenTls();
                     /*
-                     * Now resend the EHLO. Required by RFC2487 Sec. 5.2, and
-                     * more specifically, Exim.
+                     * Now resend the EHLO. Required by RFC2487 Sec. 5.2, and more specifically,
+                     * Exim.
                      */
                     result = executeSimpleCommand("EHLO " + localHost);
                 } else {
-                    if (Email.DEBUG) {
-                        Log.d(Logging.LOG_TAG, "TLS not supported but required");
+                    if (MailActivityEmail.DEBUG) {
+                        LogUtils.d(Logging.LOG_TAG, "TLS not supported but required");
                     }
                     throw new MessagingException(MessagingException.TLS_REQUIRED);
                 }
@@ -162,7 +133,6 @@ public class SmtpSender extends Sender {
              */
             boolean authLoginSupported = result.matches(".*AUTH.*LOGIN.*$");
             boolean authPlainSupported = result.matches(".*AUTH.*PLAIN.*$");
-            boolean cramMD5Supported = result.matches(".*AUTH.*CRAM-MD5.*$");
 
             if (mUsername != null && mUsername.length() > 0 && mPassword != null
                     && mPassword.length() > 0) {
@@ -172,24 +142,21 @@ public class SmtpSender extends Sender {
                 else if (authLoginSupported) {
                     saslAuthLogin(mUsername, mPassword);
                 }
-                else if (cramMD5Supported) {
-                    saslAuthCramMD5(mUsername, mPassword);
-                }
                 else {
-                    if (Email.DEBUG) {
-                        Log.d(Logging.LOG_TAG, "No valid authentication mechanism found.");
+                    if (MailActivityEmail.DEBUG) {
+                        LogUtils.d(Logging.LOG_TAG, "No valid authentication mechanism found.");
                     }
                     throw new MessagingException(MessagingException.AUTH_REQUIRED);
                 }
             }
         } catch (SSLException e) {
-            if (Email.DEBUG) {
-                Log.d(Logging.LOG_TAG, e.toString());
+            if (MailActivityEmail.DEBUG) {
+                LogUtils.d(Logging.LOG_TAG, e.toString());
             }
             throw new CertificateValidationException(e.getMessage(), e);
         } catch (IOException ioe) {
-            if (Email.DEBUG) {
-                Log.d(Logging.LOG_TAG, ioe.toString());
+            if (MailActivityEmail.DEBUG) {
+                LogUtils.d(Logging.LOG_TAG, ioe.toString());
             }
             throw new MessagingException(MessagingException.IOERROR, ioe.toString());
         }
@@ -211,22 +178,23 @@ public class SmtpSender extends Sender {
         Address[] bcc = Address.unpack(message.mBcc);
 
         try {
-            executeSimpleCommand("MAIL FROM: " + "<" + from.getAddress() + ">");
+            executeSimpleCommand("MAIL FROM:" + "<" + from.getAddress() + ">");
             for (Address address : to) {
-                executeSimpleCommand("RCPT TO: " + "<" + address.getAddress() + ">");
+                executeSimpleCommand("RCPT TO:" + "<" + address.getAddress().trim() + ">");
             }
             for (Address address : cc) {
-                executeSimpleCommand("RCPT TO: " + "<" + address.getAddress() + ">");
+                executeSimpleCommand("RCPT TO:" + "<" + address.getAddress().trim() + ">");
             }
             for (Address address : bcc) {
-                executeSimpleCommand("RCPT TO: " + "<" + address.getAddress() + ">");
+                executeSimpleCommand("RCPT TO:" + "<" + address.getAddress().trim() + ">");
             }
             executeSimpleCommand("DATA");
             // TODO byte stuffing
-            Rfc822Output.writeTo(mContext, messageId,
+            Rfc822Output.writeTo(mContext, message,
                     new EOLConvertingOutputStream(mTransport.getOutputStream()),
                     false /* do not use smart reply */,
-                    false /* do not send BCC */);
+                    false /* do not send BCC */,
+                    null  /* attachments are in the message itself */);
             executeSimpleCommand("\r\n.");
         } catch (IOException ioe) {
             throw new MessagingException("Unable to send message", ioe);
@@ -234,8 +202,9 @@ public class SmtpSender extends Sender {
     }
 
     /**
-     * Close the protocol (and the transport below it). MUST NOT return any
-     * exceptions.
+     * Close the protocol (and the transport below it).
+     *
+     * MUST NOT return any exceptions.
      */
     @Override
     public void close() {
@@ -243,10 +212,10 @@ public class SmtpSender extends Sender {
     }
 
     /**
-     * Send a single command and wait for a single response. Handles responses
-     * that continue onto multiple lines. Throws MessagingException if response
-     * code is 4xx or 5xx. All traffic is logged (if debug logging is enabled)
-     * so do not use this function for user ID or password.
+     * Send a single command and wait for a single response.  Handles responses that continue
+     * onto multiple lines.  Throws MessagingException if response code is 4xx or 5xx.  All traffic
+     * is logged (if debug logging is enabled) so do not use this function for user ID or password.
+     *
      * @param command The command string to send to the server.
      * @return Returns the response string from the server.
      */
@@ -255,13 +224,12 @@ public class SmtpSender extends Sender {
     }
 
     /**
-     * Send a single command and wait for a single response. Handles responses
-     * that continue onto multiple lines. Throws MessagingException if response
-     * code is 4xx or 5xx.
+     * Send a single command and wait for a single response.  Handles responses that continue
+     * onto multiple lines.  Throws MessagingException if response code is 4xx or 5xx.
+     *
      * @param command The command string to send to the server.
-     * @param sensitiveReplacement If the command includes sensitive data (e.g.
-     *            authentication) please pass a replacement string here (for
-     *            logging).
+     * @param sensitiveReplacement If the command includes sensitive data (e.g. authentication)
+     * please pass a replacement string here (for logging).
      * @return Returns the response string from the server.
      */
     private String executeSensitiveCommand(String command, String sensitiveReplacement)
@@ -270,12 +238,12 @@ public class SmtpSender extends Sender {
             mTransport.writeLine(command, sensitiveReplacement);
         }
 
-        String line = mTransport.readLine();
+        String line = mTransport.readLine(true);
 
         String result = line;
 
         while (line.length() >= 4 && line.charAt(3) == '-') {
-            line = mTransport.readLine();
+            line = mTransport.readLine(true);
             result += line.substring(3);
         }
 
@@ -289,26 +257,26 @@ public class SmtpSender extends Sender {
         return result;
     }
 
-    // C: AUTH LOGIN
-    // S: 334 VXNlcm5hbWU6
-    // C: d2VsZG9u
-    // S: 334 UGFzc3dvcmQ6
-    // C: dzNsZDBu
-    // S: 235 2.0.0 OK Authenticated
-    //
-    // Lines 2-5 of the conversation contain base64-encoded information. The
-    // same conversation, with base64 strings decoded, reads:
-    //
-    //
-    // C: AUTH LOGIN
-    // S: 334 Username:
-    // C: weldon
-    // S: 334 Password:
-    // C: w3ld0n
-    // S: 235 2.0.0 OK Authenticated
+
+//    C: AUTH LOGIN
+//    S: 334 VXNlcm5hbWU6
+//    C: d2VsZG9u
+//    S: 334 UGFzc3dvcmQ6
+//    C: dzNsZDBu
+//    S: 235 2.0.0 OK Authenticated
+//
+//    Lines 2-5 of the conversation contain base64-encoded information. The same conversation, with base64 strings decoded, reads:
+//
+//
+//    C: AUTH LOGIN
+//    S: 334 Username:
+//    C: weldon
+//    S: 334 Password:
+//    C: w3ld0n
+//    S: 235 2.0.0 OK Authenticated
 
     private void saslAuthLogin(String username, String password) throws MessagingException,
-            AuthenticationFailedException, IOException {
+        AuthenticationFailedException, IOException {
         try {
             executeSimpleCommand("AUTH LOGIN");
             executeSensitiveCommand(
@@ -317,63 +285,13 @@ public class SmtpSender extends Sender {
             executeSensitiveCommand(
                     Base64.encodeToString(password.getBytes(), Base64.NO_WRAP),
                     "/password redacted/");
-        } catch (MessagingException me) {
+        }
+        catch (MessagingException me) {
             if (me.getMessage().length() > 1 && me.getMessage().charAt(1) == '3') {
                 throw new AuthenticationFailedException(me.getMessage());
             }
             throw me;
         }
-    }
-
-    private void saslAuthCramMD5(String username, String password) throws MessagingException,
-            AuthenticationFailedException, IOException {
-        try {
-            String[] splitResponse = executeSimpleCommand("AUTH CRAM-MD5").split(" ");
-
-            String challenge;
-            if (splitResponse.length == 2) {
-                challenge = splitResponse[1];
-            } else {
-                throw new AuthenticationFailedException("Server challenge was malformed");
-            }
-
-            String decodedCallenge = new String(Base64.decode(challenge, Base64.NO_WRAP));
-            try {
-                Mac mac = Mac.getInstance("HmacMD5");
-                SecretKeySpec sk = new SecretKeySpec(password.getBytes(), mac.getAlgorithm());
-                mac.init(sk);
-                byte[] encryptedChallenge = mac.doFinal(decodedCallenge.getBytes());
-
-                String responseString = username + " " + bytesToHexString(encryptedChallenge);
-                String encodedResponse = Base64.encodeToString(responseString.getBytes(),
-                        Base64.NO_WRAP);
-                executeSensitiveCommand(
-                        encodedResponse,
-                        "/response redacted/");
-            } catch (NoSuchAlgorithmException e) {
-                throw new AuthenticationFailedException(
-                        "Failure during initialization of HMAC-MD5. " + e.getMessage());
-            } catch (InvalidKeyException e) {
-                throw new AuthenticationFailedException(
-                        "Failure during initialization of HMAC-MD5. " + e.getMessage());
-            }
-
-        } catch (MessagingException me) {
-            if (me.getMessage().length() > 1 && me.getMessage().charAt(1) == '3') {
-                throw new AuthenticationFailedException(me.getMessage());
-            }
-            throw me;
-        }
-    }
-
-    private String bytesToHexString(byte[] bytes) {
-        StringBuilder sb = new StringBuilder(bytes.length * 2);
-
-        Formatter formatter = new Formatter(sb);
-        for (byte b : bytes) {
-            formatter.format("%02x", b);
-        }
-        return sb.toString();
     }
 
     private void saslAuthPlain(String username, String password) throws MessagingException,
@@ -382,7 +300,8 @@ public class SmtpSender extends Sender {
         data = Base64.encode(data, Base64.NO_WRAP);
         try {
             executeSensitiveCommand("AUTH PLAIN " + new String(data), "AUTH PLAIN /redacted/");
-        } catch (MessagingException me) {
+        }
+        catch (MessagingException me) {
             if (me.getMessage().length() > 1 && me.getMessage().charAt(1) == '3') {
                 throw new AuthenticationFailedException(me.getMessage());
             }
